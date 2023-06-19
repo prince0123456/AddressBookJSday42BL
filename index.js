151f8d79dbdd18bb0ea4676607dfe5517b98e509
Prince Gupta <princegupta.pg46@gmail.com>
2023-06-19 10:25:13 +0530

    Address book Insert data

--- index.js
+++ index.js
@@ -23,3 +23,42 @@ class AddressBook{
 
     }
 }
+
+var contact1=new AddressBook("Prince","v","abc","xyz","Uttar Pradesh",201304,7703906731,"princegupta.pg46@gmail.com");//contact1.printDetails();
+var contact2=new AddressBook("vishwa","s","def","uvw","hyd",500010,8265263524,"vishwa@gmail.com");//contact2.printDetails();
+var contact3=new AddressBook("riya","k","ghi","rst","tn",500106,8237526526,"riya@gmail.com");////contact3.printDetails();
+var map1= new Map();
+map1.set(1,contact1);
+map1.set(2,contact2);
+map1.set(3,contact3);
+//console.log(map1.size);
+var array=[];
+array.push(contact1);
+array.push(contact2);
+array.push(contact3);
+console.log(array[0].firstName);
+//---------------------------------------------------------------------------
+function addDetails(){
+    var contact = new AddressBook();
+    contact.firstName = prompt("Please enter the firstName :");
+    contact.lastName = prompt("Please enter the lastName :");
+    contact.address = prompt("Please enter the address :");
+    contact.city = prompt("Please enter the city :");
+    contact.state = prompt("Please enter the state :");
+    contact.zip = prompt("Please enter the zip :");
+    contact.phoneNumber = prompt("Please enter the phoneNumber :");
+    contact.email = prompt("Please enter the email :");
+    array.push(contact);
+}
+let a;
+while(true){
+    console.log("1");//choose the option
+    var option = prompt("1)Add Details in AddressBook\n 2)Exit\n");
+    switch(option){
+        case "1":
+            addDetails();
+            break;
+       
+    }
+    
+}
